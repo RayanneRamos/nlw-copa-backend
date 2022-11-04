7ee4fca23942c4a38a0d558963a22dafeb3218c3
Rayanne Ramos <rayanneramosoficial@gmail.com>
2022-11-04 16:41:11 -0300

    Criando a rota de palpites do bolão

--- src/routes/guess.ts
+++ src/routes/guess.ts
@@ -1,10 +1,34 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
 import { prisma } from "../lib/prisma";
+import { authenticate } from "../plugins/authenticate";
 
 export async function guessRoutes(fastify: FastifyInstance) {
   fastify.get('/guesses/count', async () => {
     const count = await prisma.guess.count();
 
     return { count };
+  });
+
+  fastify.post('/pools/:poolId/games/:gameId/guesses', { onRequest: [authenticate] }, async (request, reply) => {
+    const createGuessParams = z.object({
+      poolId: z.string(),
+      gameId: z.string(),
+    });
+
+    const createGuessBody = z.object({
+      firstTeamPoints: z.number(),
+      secondTeamPoints: z.number(),
+    })
+
+    const { poolId, gameId } = createGuessParams.parse(request.params);
+    const { firstTeamPoints, secondTeamPoints } = createGuessBody.parse(request.body);
+
+    return {
+      poolId,
+      gameId,
+      firstTeamPoints,
+      secondTeamPoints,
+    }
   });
 }
